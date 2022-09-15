47853770b929b9d7057ba597ba511473af2f0b89
phunghieudong <phunghieudong1881999@gmail.com>
2022-09-15 10:02:04 +0700

    Add valus

--- src/views/Auth/Screen/Signin.tsx
+++ src/views/Auth/Screen/Signin.tsx
@@ -32,11 +32,57 @@ function SigninScreen() {
   const navigation = useNavigation<ViewProps['navigation']>(); // Hooks của navigation
   // const [text, onChangeText] = React.useState("Email");
   // const [text1, onChangeText1] = React.useState("Mật khẩu");
-  // const [number, onChangeNumber] = React.useState(null);
+  // const [number, onChangeN umber] = React.useState(null);
   const { passwordVisibility, rightIcon, handlePasswordVisibility } =
     useTogglePasswordVisibility();
-  const [password, setPassword] = useState('');
+  // const [password, setPassword] = useState('');
 
+
+  //=======================================================================
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [emailError, setEmailError] = useState("")
+  const [passwordError, setPasswordError] = useState("")
+  const handleSubmit = () => {
+    var emailValid = false;
+    if (email.length == 0) {
+      setEmailError("Email không được để trống");
+    }
+    else if (email.length < 6) {
+      setEmailError("Email phải trên 6 kí tự");
+    }
+    else if (email.indexOf(' ') >= 0) {
+      setEmailError('Email không được có khoảng trắng');
+    }
+    else {
+      setEmailError("")
+      emailValid = true
+    }
+
+    var passwordValid = false;
+    if (password.length == 0) {
+      setPasswordError("Mật khẩu không được để trống");
+    }
+    else if (password.length < 6) {
+      setPasswordError("Password phải trên 6 kí tự");
+    }
+    else if (password.indexOf(' ') >= 0) {
+      setPasswordError('Password không được có khoảng trắng');
+    }
+    else {
+      setPasswordError("")
+      passwordValid = true
+    }
+
+    if (emailValid && passwordValid) {
+      // alert('Email: ' + email + '\nPassword: ' + password);
+      {/* onPress={() => navigation.replace('Auth')} */ }
+      navigation.replace('Auth')
+      setEmail("");
+      setPassword("");
+    }
+
+  }
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
 
@@ -65,6 +111,9 @@ function SigninScreen() {
             autoCorrect={false}
             secureTextEntry={false}
             textContentType='emailAddress'
+            onChangeText={text => setEmail(text)} value={email}
+
+
             style={{
               width: '90%',
               height: 40,
@@ -73,11 +122,13 @@ function SigninScreen() {
 
             }}
 
-          // onChangeText={onChangeText1}
-          // value={text1}
+
           />
 
         </View>
+        {emailError.length > 0 &&
+          <Text style={{ color: 'red', marginTop: 6 }}>{emailError}</Text>
+        }
         <View style={{
           backgroundColor: 'white',
           width: '100%',
@@ -90,8 +141,9 @@ function SigninScreen() {
           <TextInput
             placeholder='Mật khẩu'
             autoCorrect={false}
-            secureTextEntry={false}
+            secureTextEntry={true}
             textContentType='username'
+            onChangeText={text => setPassword(text)} value={password}
             style={{
               width: '90%',
               height: 40,
@@ -103,20 +155,26 @@ function SigninScreen() {
           // onChangeText={onChangeText1}
           // value={text1}
           />
+
           <TouchableOpacity>
             <Pressable onPress={handlePasswordVisibility}>
               <FontAwesome name='eye-slash' size={22} color="#232323" />
             </Pressable>
           </TouchableOpacity>
         </View>
+        {passwordError.length > 0 &&
+
+          <Text style={{ color: 'red', marginTop: 6 }}>{passwordError}</Text>
+        }
       </View>
       <View style={{ marginTop: 16, width: 343, flexDirection: "row-reverse" }}>
         <TouchableOpacity onPress={() => navigation.navigate('ForgotPassword')}>
-          <Text style={{ color: 'red', fontStyle:'italic' }}>Quên mật khẩu</Text>
+          <Text style={{ color: 'red', fontStyle: 'italic' }}>Quên mật khẩu</Text>
 
         </TouchableOpacity>
       </View>
-      <TouchableOpacity onPress={() => navigation.replace('Auth')}>
+      {/* onPress={() => navigation.replace('Auth')} */}
+      <TouchableOpacity onPress={handleSubmit}>
         {/* Chổ này mà xài replace là đá thẳng ra luôn */}
         <View style={{ marginTop: 16, borderColor: "#000000", backgroundColor: '#9CBD44', height: 44, width: 343, borderRadius: 6, justifyContent: 'center', alignItems: 'center' }}>
           <Text style={{ fontSize: 16, color: "#ffffff", fontWeight: "600" }}>ĐĂNG NHẬP</Text>

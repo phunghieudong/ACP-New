c7e0463f6fbd9a00ba72e4139122a3ced8f5e6fc
phunghieudong <phunghieudong1881999@gmail.com>
2022-10-13 13:34:10 +0700

    Add

--- src/views/Root/Screen/Profile/ConfirmFail.tsx
+++ src/views/Root/Screen/Profile/ConfirmFail.tsx
@@ -1,43 +1,28 @@
 
-
 //@ts-nocheck
 import React, { useEffect, useState } from 'react';
 import { Text, View, StyleSheet, Image, ScrollView, TouchableOpacity, TextInput } from 'react-native';
 import HeaderRoot from "../../../../components/HeaderRoot/index";
-
 const ConfirmFailScreen = ({ navigation }) => {
-
-
-
     return (
         <View style={styles.container}>
             <View style={{ backgroundColor: '#9CBD44', width: '100%', height: 64, flexDirection: 'row', justifyContent: 'space-between', paddingHorizontal: 25, paddingTop: 25 }}>
-
                 <View style={{ flexDirection: 'row', width: 50 }}>
-
-
                 </View>
-
                 <View >
                     <Text style={{ fontSize: 20, color: '#ffffff', fontWeight: '600' }}>Đăng ký thất bại</Text>
                 </View>
-
                 <View style={{ flexDirection: 'row', height: 10, width: 50 }}>
-
                 </View>
-
             </View>
             <View style={{ marginHorizontal: 20, marginTop: 32, justifyContent: 'center', alignItems: 'center', alignSelf: 'center' }}>
                 <Image
-
                     source={require('../../../../assets/images/Confirm1.png')}
                     style={{ width: 87.69, height: 78.85 }}
                 />
-
                 <Text style={{ fontSize: 20, color: '#000000', marginTop: 34, fontWeight: "600", textAlign: 'center' }}>Bạn đã đấu thầu của phiên này!</Text>
                 <Text style={{ fontSize: 14, color: '#999999', marginTop: 8, textAlign: 'center' }}>Quay trở lại trang chủ để tiếp tục đấu các phiên thầu trong tương lai .</Text>
                 {/* <Text style={{ fontSize: 14, color: '#999999', marginTop: 8 }}></Text> */}
-
             </View>
             <View style={{ justifyContent: 'center', alignItems: 'center', marginTop: 32, width: "100%", }}>
                 <TouchableOpacity onPress={() => navigation.navigate('HomeScreen')}>
@@ -49,15 +34,10 @@ const ConfirmFailScreen = ({ navigation }) => {
         </View>
     );
 };
-
-
-
 const styles = StyleSheet.create({
-
     container: {
         flex: 1,
         backgroundColor: '#fff',
-
     },
     text: {
         fontSize: 22,

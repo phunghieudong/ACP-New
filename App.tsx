800b00dc9544c3012d789a2e03ce5a916e24ad15
phunghieudong <phunghieudong1881999@gmail.com>
2022-10-14 13:28:44 +0700

    Add

--- App.tsx
+++ App.tsx
@@ -1,17 +1,26 @@
 //@ts-nocheck
-import React from 'react';
-
-import { Provider as StoreProvider } from 'react-redux';
+import React, { useEffect } from "react";
+import OneSignal from "react-native-onesignal";
+import Constants from "expo-constants";
+import { Provider as StoreProvider } from "react-redux";
 import RootNavigator from "./src/navigators/rootNavigator";
-import store from './src/store';
-import { LogBox } from "react-native";
+import store from "./src/store";
+import { LogBox, Text } from "react-native";
+
 
 LogBox.ignoreAllLogs();
 const App = () => {
+  useEffect(() => {
+    OneSignal.setAppId("300d518d-1d40-4a06-8f46-eeb920a7bc52");
+  }, []);
   return (
-    <StoreProvider store={store}>
-      <RootNavigator />
-    </StoreProvider>
+    <>
+    
+      <StoreProvider store={store}>
+        <RootNavigator />
+      </StoreProvider>
+      <Text>300d518d-1d40-4a06-8f46-eeb920a7bc52</Text>
+    </>
   );
 };
 
